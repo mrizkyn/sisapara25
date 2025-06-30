a37d28090f418ed783c1f5e7b17370270f8905f7
mrizkyn <m.rizky.n9190@gmail.com>
2025-06-30 12:37:39 +0700

    revisi 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
     <div class="auth Patterns">
 
-        <div class="auth-container">
+        <div class="auth-container" style="margin-bottom: 200px; margin-top: 100px;">
             <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
             <div class="auth-form">
                 <p class="auth-title">Create Account</p>
